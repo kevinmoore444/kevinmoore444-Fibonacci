799f48ac2e03221aaccfa7dd0ab9e012411c1bf6
Kevin Moore <kevinmoore444@gmail.com>
2023-07-11 20:32:41 +0000

    commit

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -5,7 +5,7 @@ public class Fibonacci {
      * The fibonacci sequence is calculated by adding the previous two numbers in the sequence to obtain the next
      * number. The first two numbers in the whole fibonacci sequence are always 0 and 1.
      * For instance, the sequence starts as:
-     * 0 1 1 2 3 
+     * 0 1 1 2 3 5
      *            
      * where the 4th number of the sequence would be 3, the 5th number would be 5, the 6th number would be 8, etc.
      *
